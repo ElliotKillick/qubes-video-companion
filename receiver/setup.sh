2a0cbdefb67f31144acf91236e3d5d51f9cc7bde
Elliot Killick <elliotkillick@zohomail.eu>
2021-06-18 13:19:48 -0400

    Fix typos

--- receiver/setup.sh
+++ receiver/setup.sh
@@ -19,7 +19,7 @@ fi
 # exclusive_caps=1: Some applications such as Cheese and Chromium won't detect the video device if it's set to zero
 sudo modprobe v4l2loopback card_label="Qubes Video Companion" exclusive_caps=1
 
-# For some reason, AppVMs based off my self-made "kali" qube (which itself is based off the "debian-10" TemplateVM) that are using the 5.x Qubes Linux kernel no longer hasthe user permitting ACL (or any ACL for that matter) on /dev/video* devices causing a permission error when attempting to write video to the device
+# For some reason, AppVMs based off my self-made "kali" qube (which itself is based off the "debian-10" TemplateVM) that are using the 5.x Qubes Linux kernel no longer has the user permitting ACL (or any ACL for that matter) on /dev/video* devices causing a permission error when attempting to write video to the device
 # As a workaround, we set the ACL ourselves in case it isn't already applied
 # This issue does not occur on the Fedora or Debian AppVMs using the 5.x Qubes Linux kernel, more research is required
 sudo setfacl -m user:user:rw /dev/video0
